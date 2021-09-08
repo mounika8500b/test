ea4bd649f25e866636b4ef41b4d552021cdc9d40
mounika8500b <90328962+mounika8500b@users.noreply.github.com>
2021-09-08 18:55:09 +0530

    1st commit

---
+++ copy.ps1
@@ -0,0 +1,9 @@
+﻿$remoteFilePath = 'C:\Intel'
+Copy-Item -Path C:\Users\Mounika.Bethu\email_cred.txt -Destination $remoteFilePath
+Write-Host "I've just copied the file to $remoteFilePath"
+
+$VerbosePreference = "Continue"
+$LogPath = Split-Path $MyInvocation.MyCommand.Path
+Get-ChildItem "$LogPath\*.log" | Where LastWriteTime -LT (Get-Date).AddDays(-15) | Remove-Item -Confirm:$false
+$LogPathName = Join-Path -Path $LogPath -ChildPath "$($MyInvocation.MyCommand.Name)-$(Get-Date -Format 'MM-dd-yyyy').log"
+Start-Transcript $LogPathName -Append
